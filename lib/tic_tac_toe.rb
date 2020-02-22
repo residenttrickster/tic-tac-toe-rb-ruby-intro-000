f9aa2878279fe78be21907434b6c60471a4fa674
Anastasia Orlova <metkusblackus@gmail.com>
2020-02-22 03:09:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -124,4 +124,11 @@ def play(board)
   until over?(board) == true
     turn(board)
   end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
 end 
+
+
